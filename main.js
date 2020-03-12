4003d9b854e46438be73de6bc3b657fa07344899
tilenh <tilen.heco@gmail.com>
2020-03-12 21:23:29 +0100

    commit

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-async function getData() {
-  const response = await fetch(
-    "api.openweathermap.org/data/2.5/weather?q=2172797&appid=512f732a2036f5fd0ed45914fe1daf90"
-  );
-  //const data = response.json();
-  console.log(response);
+let weather = {};
+const input = document.getElementById("search");
+const submit = document.getElementById("submit");
+const div = document.querySelector("div");
+submit.addEventListener("click", getData);
+
+function toCelsius(k) {
+  k = k - 273.15;
 }
 
-getData();
+async function getData(e) {
+  e.preventDefault();
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&APPID=512f732a2036f5fd0ed45914fe1daf90`
+  );
+  const data = await response.json();
+  console.log(data);
+  let temp = await (data.main.temp -= 273.15);
+  temp = await Math.round(temp);
+  weather = {
+    name: data.name,
+    temp: temp,
+    country: data.sys.country,
+    main: data.weather[0].main
+  };
+  div.innerHTML = `<h2>${weather.name}, ${weather.country}</h2><p>${weather.temp}°</p><p>${weather.main}</p>`;
+  console.log(weather);
+}
